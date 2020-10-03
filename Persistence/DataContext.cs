53fda45ce0f632f1cf2ec128d05942bf31be2167
Mohammad Alaavi <m.alaavi@tagrow.net>
2020-10-03 17:06:01 +0330

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions option) : base(option)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+            .HasData(
+                new Value { Id = 1, Name = "Mohammad" },
+                new Value { Id = 2, Name = "Leila" },
+                new Value { Id = 3, Name = "Zeynol" }
+            );
+        }
+    }
+}
